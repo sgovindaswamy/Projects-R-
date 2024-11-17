646f19f4c988043176f6671a194d4e8c1636fe92
sgovindaswamy <sanjanagovindaswamy@gmail.com>
2024-11-17 23:44:07 +0100

    Update
    Update

--- Survival_analysis/Survival_analysis.R
+++ Survival_analysis/Survival_analysis.R
@@ -5,6 +5,7 @@ library(earth)
 library(pROC)
 library(ggplot2)
 library(survival)
+library(survminer)
 
 setwd("C:/Users/sanja/Documents/GitHub/Projects-R-/Survival_analysis")
 current_dir <- getwd()
@@ -42,14 +43,19 @@ data$Relapse.Free.Status <- ifelse(data$Relapse.Free.Status == "Recurred", 0, 1)
 surv_object_relapse <- Surv(data$Relapse.Free.Status..Months., data$Relapse.Free.Status)
 km_fit_relapse <- survfit(surv_object_relapse ~ 1) # 1 denotes we are calculating the kaplan Meier curve without including any predictor
 png("KaplanMeier_relapse_plot.png", width = 1200, height = 800)
-plot(
-  km_fit_relapse,
+# Plot the Kaplan-Meier curve with a risk table
+ggsurvplot(
+  km_fit_relapse,  # The Kaplan-Meier fit object
+  data = data,  # The data used to create the survival object
   xlab = "Time in months",
   ylab = "Probability of relapse to not recurr",
-  main = "Kaplan-Meier relapse Curves",
-  col = "blue",  
-  lty = 1,       
-  conf.int = TRUE
+  main = "Kaplan-Meier Relapse Curve",
+  col = "blue",  # Line color for the curve
+  lty = 1,       # Line type for the curve
+  conf.int = TRUE,  # Show confidence interval
+  risk.table = TRUE,  # Add risk table below the curve
+  risk.table.col = "strata",  
+  ggtheme = theme_minimal()  # Use a minimal theme for the plot
 )
 dev.off()
 
@@ -58,13 +64,17 @@ data$Overall.Survival.Status <- ifelse(data$Overall.Survival.Status == "Living",
 surv_object_survival <- Surv(data$Overall.Survival..Months., data$Overall.Survival.Status)
 km_fit_surv <- survfit(surv_object_survival ~ 1)
 png("kaplanMeier_survival_plot.png", width = 1200, height = 800)
-plot(
-  km_fit_surv,
+# Plot the Kaplan-Meier curve with a risk table
+ggsurvplot(
+  km_fit_surv,  # The Kaplan-Meier fit object
+  data = data,  # The data used to create the survival object
   xlab = "Time in months",
   ylab = "Probability of deadth to occur",
-  main = "Kaplan-Meier Survival Curves",
-  col = "blue",  
-  lty = 1,       
-  conf.int = TRUE
-) 
+  main = "Kaplan-Meier Relapse Curve",
+  col = "blue",  # Line color for the curve
+  lty = 1,       # Line type for the curve
+  conf.int = TRUE,  # Show confidence interval
+  risk.table = TRUE,  # Add risk table below the curve
+  ggtheme = theme_minimal()  
+)
 dev.off()
